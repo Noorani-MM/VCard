5a733cd724eacf69307853bad362b76f00470f81
matio <matio.nora2000@gmail.com>
2023-03-20 01:18:30 +0330

    feat-page: service page generated

---
+++ Front-end/pages/Service.vue
@@ -0,0 +1,39 @@
+<template>
+    <Detail title="SERVICE" description="What I do">
+        <div>
+            <h1 class="font-semibold">Programming</h1>
+            <p class="px-2">
+                <span>
+                    I am a programmer so I can code!<br />
+                    I can do coding with this platforms <br />
+                </span>
+                <div class="text-black/75 p-5">
+                    <div class="grid grid-cols-2">
+                        <div>
+                            <label>PHP</label>
+                        </div>
+                        <div>
+                            <span>95%</span>
+                        </div>
+                    </div>
+                    <div class="grid grid-cols-2">
+                        <div>
+                            <label>C#</label>
+                        </div>
+                        <div>
+                            <span>90%</span>
+                        </div>
+                    </div>
+                    <div class="grid grid-cols-2">
+                        <div>
+                            <label>JavaScript</label>
+                        </div>
+                        <div>
+                            <span>95%</span>
+                        </div>
+                    </div>
+                </div>
+            </p>
+        </div>
+    </Detail>
+</template>
