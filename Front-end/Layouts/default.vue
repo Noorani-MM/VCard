98eaf749e6f4a0c750374e5eb608874c3f7f7154
matio <matio.nora2000@gmail.com>
2023-03-20 00:40:04 +0330

    feat-page: about page generated

---
+++ Front-end/Layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="min-h-screen bg-[url('/Images/Background.jpg')] bg-cover">
+    <div class="pt-12"></div> <!-- empty space -->
+    <MyHeader>
+        <Navbar /> 
+    </MyHeader>
+
+    <Detail title="ABOUT" description="A brief info about me">
+        <RouterView />
+    </Detail>
+    </div>
+</template>
+
+<script setup>
+console.log("layouts");
+</script>
+  
---
+++ Front-end/pages/About.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex flex-row">
+        <div class="col-span-2 w-2/3">
+            Hello my dear friends. I am <span class="font-bold">MOHAMMAD MAHDI NOORANI</span>.
+            I am Backend developer and I working with laravel fream work! <br />
+            <span class="font-bold font-serif">I AM MASTER AT MY WORK !!</span>
+        </div>
+        <div class="w-1/3">
+            <img class="w-full h-[14rem] object-cover" src="/Images/Background.jpg" alt="my picture" />
+        </div>
+    </div>
+</template>
