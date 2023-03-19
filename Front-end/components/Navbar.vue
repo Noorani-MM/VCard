2ef94d6869afa97c35fa4f2df0ff3ea57d910d6f
matio <matio.nora2000@gmail.com>
2023-03-19 23:12:06 +0330

    feat-component: navbar item component created

--- Front-end/components/Navbar.vue
+++ Front-end/components/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
-    <nav class="p-2">
+    <nav class="p-2 ">
       <ul class="flex flex-row gap-5">
-        <li class="text-xl font-bold font-serif p-2 border border-slate-400 rounded-xl hover:bg-slate-600 hover:text-white cursor-pointer sm:text-2xl">Home</li>
-        <li class="text-xl font-bold font-serif p-2 border border-slate-400 rounded-xl hover:bg-slate-600 hover:text-white cursor-pointer sm:text-2xl">Profile</li>
-        <li class="text-xl font-bold font-serif p-2 border border-slate-400 rounded-xl hover:bg-slate-600 hover:text-white cursor-pointer sm:text-2xl">Contact</li>
+        <NavbarItem title="ABOUT" />
+        <NavbarItem title="SERVICE" />
+        <NavbarItem title="CONTACT" />
       </ul>
     </nav>
 </template>
---
+++ Front-end/components/Navbar/Item.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="text-xl text-black/50 font-serif p-2 hover:text-black hover:underline underline-offset-8 cursor-pointer sm:text-2xl">
+        <NuxtLink to="#">{{ props.title }}</NuxtLink>
+    </div>
+</template>
+
+<script setup>
+    const props = defineProps({
+        title : {
+            type : String,
+            required: true
+        }
+    });
+</script>
