bf1e78a534917d5d0c63149253a9f3a928fe542e
Mohammad Mahdi Noorani <matio.nora2000@gmail.com>
2023-06-12 17:59:51 +0430

    fix-page: index page style fixed to text-justofy

--- Front-end/pages/index.vue
+++ Front-end/pages/index.vue
@@ -1,9 +1,8 @@
 <template>
-  <Detail title="HOME" description="Here you can know more about me !">
+  <Detail title="HOME" description="A brief info about me.">
     <div class="card card-side bg-base-100">
       <div class="card-body w-3/4">
-        <h2 class="card-title">How long I'm coding ?</h2>
-        <p class="text-sm sm:text-lg">
+        <p class="text-sm sm:text-lg text-justify">
           I started coding in 2018. My career started with C# and after 3 years
           I switched to PHP. <br />
           I have been using Laravel framework and writing API for almost 1 year
