9da83be00279531c83ab991a7cddf02478963fd2
Mohammad Mahdi Noorani <matio.nora2000@gmail.com>
2023-06-12 22:24:58 +0430

    feat-component: add rating component

--- Front-end/components/Detail.vue
+++ Front-end/components/Detail.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="bg-white/95 pt-4 m-2">
         <div class="grid grid-cols-8 gap-4 sm:gap-6 font-serif">
-            <div class="col-span-3 bg-black text-white text-3xl sm:text-4xl font-bold p-2 grid justify-center">
+            <div class="col-span-3 bg-black text-white uppercase text-3xl sm:text-4xl font-bold p-2 grid justify-center">
                 {{ title }}
             </div>
             <div class="col-span-5 flex items-center ">
