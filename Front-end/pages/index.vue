6a71a66c2af71d2a2f3358a9ba032ca72d06d5b5
Mohammad Mahdi Noorani <matio.nora2000@gmail.com>
2023-06-12 22:45:25 +0430

    refactor-page: index page content refactor

--- Front-end/pages/index.vue
+++ Front-end/pages/index.vue
@@ -1,17 +1,17 @@
 <template>
-  <Detail title="HOME" description="A brief info about me.">
-    <div class="card card-side bg-base-100">
-      <div class="card-body w-3/4">
+  <Detail title="HOME" description="A brief info about me."
+    ><div class="card bg-base-100 image-full">
+      <figure>
+        <img src="/images/software.jpg" alt="about" />
+      </figure>
+      <div class="card-body">
         <p class="text-sm sm:text-lg text-justify">
-          &nbsp;I started coding in 2018. My career started with C# and after 3 years
-          I switched to PHP. <br />
-          &nbsp;I have been using Laravel framework and writing API for almost 1 year
-          now.
+          &nbsp;I started coding in 2018. My career started with C# and after 3
+          years I switched to PHP. <br />
+          &nbsp;I have been using Laravel framework and writing API for almost 1
+          year now.
         </p>
       </div>
-      <figure class="w-1/4">
-        <img src="https://picsum.photos/id/1/200/300" alt="Noorani" />
-      </figure>
     </div>
   </Detail>
 </template>
