d13f779095f472fc4cd77679e76b6623efecd53f
Mohammad Mahdi Noorani <matio.nora2000@gmail.com>
2023-06-12 17:39:16 +0430

    feat-page: index page content created

--- Front-end/pages/index.vue
+++ Front-end/pages/index.vue
@@ -1,5 +1,18 @@
 <template>
-    <Detail title="HOME" description="Here you can know more about me !">
-        Home page
-    </Detail>
+  <Detail title="HOME" description="Here you can know more about me !">
+    <div class="card card-side bg-base-100">
+      <div class="card-body w-3/4">
+        <h2 class="card-title">How long I'm coding ?</h2>
+        <p class="text-sm sm:text-lg">
+          I started coding in 2018. My career started with C# and after 3 years
+          I switched to PHP. <br />
+          I have been using Laravel framework and writing API for almost 1 year
+          now.
+        </p>
+      </div>
+      <figure class="w-1/4">
+        <img src="https://picsum.photos/id/1/200/300" alt="Noorani" />
+      </figure>
+    </div>
+  </Detail>
 </template>
