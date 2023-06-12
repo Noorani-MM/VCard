2b9a0f8f080839c43e8368e9c51200d7e65c489b
Mohammad Mahdi Noorani <matio.nora2000@gmail.com>
2023-06-12 18:00:19 +0430

    feat-page: about page content created and need to change picture

--- Front-end/pages/About.vue
+++ Front-end/pages/About.vue
@@ -1,14 +1,23 @@
 <template>
-    <Detail title="ABOUT" description="A brief info about me">
-        <div class="flex flex-row">
-        <div class="col-span-2 w-2/3">
-            Hello my dear friends. I am <span class="font-bold">MOHAMMAD MAHDI NOORANI</span>.
-            I am Backend developer and I working with laravel fream work! <br />
-            <span class="font-bold font-serif">I AM MASTER AT MY WORK !!</span>
-        </div>
-        <div class="w-1/3">
-            <img class="w-full h-[14rem] object-cover" src="/Images/Background.jpg" alt="my picture" />
-        </div>
+  <Detail title="ABOUT" description="Here you can know more about me !">
+    <div class="card card-side bg-base-100">
+      <div class="card-body w-3/4">
+        <p class="text-sm sm:text-lg text-justify">
+          <span>
+            &nbsp;I am energetic, creative, frank and love solving problems and new
+            challenges.
+          </span>
+          <br />
+          <span>
+            &nbsp;I enjoy working with people and teams who are crazy about
+            improvement, I try to improve with the team and also make the team
+            improve.
+          </span>
+        </p>
+      </div>
+      <figure class="w-1/4">
+        <img src="https://picsum.photos/id/1/200/300" alt="Noorani" />
+      </figure>
     </div>
-    </Detail>
+  </Detail>
 </template>
