78e5c166aaef31664f65bc757f73c55cb279c5a9
Mohammad Mahdi Noorani <matio.nora2000@gmail.com>
2023-06-13 12:27:21 +0430

    feat-page: CardBackgrounded component sets as LazyCardBackgrounded

--- Front-end/pages/Skills.vue
+++ Front-end/pages/Skills.vue
@@ -1,6 +1,6 @@
 <template>
   <Detail title="skills" description="What I do as a software engineer">
-    <CardBackgrounded background="/Images/skill.jpg" alt="skills">
+    <LazyCardBackgrounded background="/Images/skill.jpg" alt="skills">
       <h2 class="card-title">Programming</h2>
       <table class="table table-xs sm:table-md">
         <thead>
@@ -64,6 +64,6 @@
           </tr>
         </tbody>
       </table>
-    </CardBackgrounded>
+    </LazyCardBackgrounded>
   </Detail>
 </template>
