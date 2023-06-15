6b76d0a81204a3090b3e111ad29321a31dea9903
Mohammad Mahdi Noorani <matio.nora2000@gmail.com>
2023-06-15 17:37:16 +0330

    fix-component: content component fixed title tag

--- Front-end/components/Content.vue
+++ Front-end/components/Content.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="bg-white/95 pt-4 m-2">
         <div class="grid grid-cols-8 gap-4 sm:gap-6 font-serif">
-            <div class="col-span-3 bg-black text-white uppercase text-3xl sm:text-4xl font-bold p-2 grid justify-center">
+            <h3 class="col-span-3 bg-black text-white uppercase text-3xl sm:text-4xl font-bold p-2 grid justify-center">
                 {{ title }}
-            </div>
+            </h3>
             <div class="col-span-5 flex items-center ">
                 <span class="w-full">
                     {{  description }}
