8fa91ce6461803aad8369199fc73975187bba460
Mohammad Mahdi Noorani <matio.nora2000@gmail.com>
2023-06-15 17:31:56 +0330

    refactor-layout: default layout header refactored

--- Front-end/Layouts/default.vue
+++ Front-end/Layouts/default.vue
@@ -1,30 +1,48 @@
 <template>
-  <div class="min-h-screen bg-[url('/Images/Background.jpg')] bg-cover">
+  <div class="min-h-screen">
     <div class="flex justify-start">
       <div class="w-[32rem] sm:w-[38rem] mx-auto md:mx-0">
         <div class="pt-12"></div>
         <!-- empty space -->
-        <MyHeader>
-          <Navbar>
-            <NavbarItem link="/" title="HOME" :active="check_active_page('')" />
-            <NavbarItem
-              link="/about"
-              title="ABOUT"
-              :active="check_active_page('about')"
-            />
-            <NavbarItem
-              link="/contact"
-              title="Contact"
-              :active="check_active_page('contact')"
-            />
-            <NavbarItem
-              link="/skills"
-              title="skills"
-              :active="check_active_page('skills')"
-            />
-          </Navbar>
-        </MyHeader>
-        <NuxtPage />
+        <div class="bg-white/95 pt-4 m-2">
+          <div class="pl-4">
+            <div class="text-black/50 font-bold font-serif mb-4">
+              <span class="text-xl sm:text-2xl">HELLO. MY NAME IS</span>
+              <h1 class="text-black text-2xl sm:text-3xl">
+                MOHAMMAD MAHDI NOORANI
+              </h1>
+              <span class="text-lg sm:text-xl">I AM BACKEND DEVELOPER</span>
+            </div>
+          </div>
+          <hr class="mx-4 border-black/50" />
+          <div class="px-5 w-full">
+            <Navbar>
+              <NavbarItem
+                link="/"
+                title="HOME"
+                :active="check_active_page('')"
+              />
+              <NavbarItem
+                link="/about"
+                title="ABOUT"
+                :active="check_active_page('about')"
+              />
+              <NavbarItem
+                link="/contact"
+                title="Contact"
+                :active="check_active_page('contact')"
+              />
+              <NavbarItem
+                link="/skills"
+                title="skills"
+                :active="check_active_page('skills')"
+              />
+            </Navbar>
+          </div>
+        </div>
+        <div>
+          <NuxtPage />
+        </div>
       </div>
     </div>
   </div>
@@ -41,9 +59,9 @@ const check_active_page = (page) => {
   return page === current_page();
 };
 
-addRouteMiddleware((to, from) => {  
+addRouteMiddleware((to, from) => {
   let baseTitle = "Noorani";
-  let page = to.fullPath.split('/')[1];
+  let page = to.fullPath.split("/")[1];
   if (page === "") {
     page = "home";
   }
