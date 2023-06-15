78abc4aac179570988eaa635d7dacd016ccc25d4
Mohammad Mahdi Noorani <matio.nora2000@gmail.com>
2023-06-15 17:44:53 +0330

    refactor-page: index page content refactored

--- Front-end/pages/index.vue
+++ Front-end/pages/index.vue
@@ -2,7 +2,8 @@
   <Content title="HOME" description="A brief info about me.">
     <CardBackgrounded background="/Images/software.jpg" alt="Software Engineer">
       <p class="text-sm sm:text-lg text-justify">
-        &nbsp;I started coding in 2018. My career started with C# and after 3
+        &nbsp;Hello, I am Mohammad Mahdi Noorani, I'm from Iran and I'm developer.
+        I started coding in 2018. My career started with C# and after 3
         years I switched to PHP. <br />
         &nbsp;I have been using Laravel framework and writing API for almost 1
         year now.
