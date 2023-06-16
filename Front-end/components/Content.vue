d064310089b1d8ab5207933524c20c35f0173b0c
Mohammad Mahdi Noorani <matio.nora2000@gmail.com>
2023-06-16 19:01:01 +0330

    fix-component: content component tags fixed

--- Front-end/components/Content.vue
+++ Front-end/components/Content.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white/95 pt-4 mt-2">
+  <div class="pt-4 mt-2">
     <div class="grid grid-cols-8 gap-4 sm:gap-6 font-serif">
       <h2
         class="col-span-3 bg-black text-white uppercase text-3xl sm:text-4xl font-bold p-2 grid justify-center"
