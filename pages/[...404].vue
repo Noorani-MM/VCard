bb6fad52539a95a96ab77042327a83e8099766ee
Mohammad Mahdi Noorani <matio.nora2000@gmail.com>
2023-06-20 14:36:25 +0330

    feat-config: [404] page error configured

---
+++ pages/[...404].vue
@@ -0,0 +1,8 @@
+<template>
+  <Content title="404" description="[404] Page not found.">
+    <div class="flex content-center w-full h-full p-2">
+      <img src="/Images/404.jpg" class="rounded-md mx-auto" />
+    </div>
+  </Content>
+</template>
+
