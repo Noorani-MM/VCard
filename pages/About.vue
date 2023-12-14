ecc6ad4ff229ff85f7e031304f77969a81c5d55a
Mohammad Mahdi Noorani <matio.nora2000@gmail.com>
2023-12-14 06:07:48 +0330

    About page override

--- pages/About.vue
+++ pages/About.vue
@@ -1,25 +1,32 @@
 <template>
-  <Content
-    :title="config.__(`${page}/title`.split('/'), null, lang)"
-    :description="config.__(`${page}/description`.split('/'), null, lang)"
-  >
-    <CardBackgrounded background="/Images/about.jpg" alt="about">
-      <p class="text-sm sm:text-lg text-justify" v-html="config.__(`${page}/text`.split('/'), null, lang)">
-      </p>
-    </CardBackgrounded>
-  </Content>
+  <div class="flex flex-col gap-2 w-full h-full">
+    <!-- header section -->
+    <div class="flex flex-row items-center gap-3 font-serif">
+      <h2 class="flex items-center justify-center px-3 py-2 bg-base-300 text-2xl font-semibold w-56">About</h2>
+      <span class="flex flex-row items-center px-2 py-1 border-b border-b-base-content w-[80vh]">
+        Here you can know more about me !
+      </span>
+    </div>
+
+    <!-- page content -->
+    <div class="flex flex-col w-full items-center h-5/6">
+      <div class="relative flex flex-col w-11/12 rounded-lg h-full overflow-clip">
+        <NuxtImg class="absolute top-0 left-0 w-full h-full object-cover" src="/Images/about.jpg"
+          alt="about me" />
+        <div class="flex flex-col gap-1 w-full h-full bg-base-200/30 z-10 px-5 py-6 md:py-8 md:px-7">
+          <p class="text-justify">
+            &nbsp;I am energetic, creative, frank and love solving problems and new challenges.
+          </p>
+          <p>
+            &nbsp;I enjoy working with people and teams who are crazy about improvement, I try to improve with the team and also make the team improve.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import Config from "~~/composables/Config";
-const props = defineProps({
-  lang: {
-    type: String,
-    default: "en",
-  },
-});
-
-const page = "pages/about";
 useHead({
   meta: [
     {
@@ -31,7 +38,7 @@ useHead({
       name: "title",
       property: "og:title",
       content: "What kind of person am I?",
-    },
+    }
   ],
 });
 </script>
